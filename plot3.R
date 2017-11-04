faacb94ed6d2c776e2fd8e4d1dbde07561891c9b
katiesevans <Kathryn.Evans@u.northwestern.edu>
2017-11-04 11:23:30 -0500

    add plots and code

---
+++ plot3.R
@@ -0,0 +1,28 @@
+library(data.table)
+library(lubridate)
+library(dplyr)
+setwd("~/Documents/Work/ExData_Plotting1/")
+
+#Download, unzip, and read data file
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+              destfile = "power_consumption.zip")
+powerdf <- read.table(unz("power_consumption.zip", "household_power_consumption.txt"), sep = ";", 
+                      na.strings = "?", header = TRUE)
+
+#Subset dataframe to keep only 2007-02-1 thru 2007-02-02
+powerdf$Date <- as.Date(powerdf$Date, "%d/%m/%Y")
+
+feb2007 <- powerdf %>%
+  dplyr::filter(Date %in% c(as.Date("2007-02-01"), as.Date("2007-02-02"))) %>%
+  dplyr::mutate(datetime = as.POSIXct(strptime(paste(Date, Time), "%Y-%m-%d %H:%M:%S")))
+
+#plot global active power histogram
+png("plot3.png")
+with(feb2007, plot(datetime, Sub_metering_1, type = "n", 
+                   ylab = "Energy sub metering", xlab = ""))
+lines(x = feb2007$datetime, y = feb2007$Sub_metering_1)
+lines(x = feb2007$datetime, y = feb2007$Sub_metering_2, col = "red")
+lines(x = feb2007$datetime, y = feb2007$Sub_metering_3, col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col = c("black", "red", "blue"), lty = 1)
+dev.off()
